c22e9a80cc01fe5ad4458b001c54f58b0bff8da5
manu <m.iglesias91@gmail.com>
2020-10-19 18:42:16 -0300

    parametros de 'ranger' desde comando.

--- scripts/ranger.R
+++ scripts/ranger.R
@@ -13,24 +13,28 @@ args = commandArgs(trailingOnly=TRUE)
 # PARA DEBUG
 # args = c('iguala', '201911', '202001', '~/Documentos/maestria-dm/dm-eyf/datasets/paquete_premium_201906_202001.txt.gz', '~/Documentos/maestria-dm/dm-eyf/kaggle/ranger_basico.csv')
 
-# test if there is at least one argument: if not, return an error
-path_salida = '~/ranger_basico.csv'
-if ( length(args) < 4 || length(args) > 5) {
-  stop("Tienen que ser 4 o 5 parametros:
+if (  length(args) != 9) {
+  stop("Tienen que ser 9 parametros:
   1: 'mayora', 'menora' o 'iguala']
   2: meses de entrenamiento: 201911, 201910, ...
   3: mes de evaluacion: 202001, 201911, ...
   4: path dataset entrada: '~/paquete_premium_201906_202001.txt.gz'
-  5: [OPCIONAL] path de salida: '~/ranger.csv'", call.=FALSE)
-}
-if (length(args) == 5) {
-  path_salida = args[5]
+  5: path de salida: '~/ranger.csv'
+  6: n_arboles: 100, 200, ..., 900 ...
+  7: n_split: 2, 3, ... , 20...
+  8: nodo_min: 1, 2, ... , 40
+  9: profundidad_max: 0, 1, 2, ..., 20, 21,... ", call.=FALSE)
 }
 
 comparacion = args[1]
 foto_mes_entrenamiento = as.integer(args[2])
 foto_mes_evaluacion = as.integer(args[3])
 dataset_path = args[4]
+path_salida = args[5]
+n_arboles = args[6]
+n_split = args[7]
+nodo_min = args[8]
+profundidad = args[9]
 
 # levanto dataset
 dataset = levantar_clientes(path = dataset_path, nombre_clase_binaria = 'baja', positivo = 'si', negativo = 'no', fix_nulos = T)
@@ -46,6 +50,6 @@ if (comparacion == 'mayora') {
 }
 
 rutiles::rf_ranger(a_predecir = 'baja', positivo = 'si',
-                   n_arboles = 500, n_split = 3, nodo_min = 1, profundidad_max = 0,
+                   n_arboles = n_arboles, n_split = n_split, nodo_min = nodo_min, profundidad_max = profundidad_max,
                    salida_csv = path_salida,
                    entrenamiento = entrenamiento, evaluacion = dataset[foto_mes == foto_mes_evaluacion, ])
