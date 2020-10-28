c44efd80e40dc05150b8935aa7ff3e9bcf638a9d
manu <m.iglesias91@gmail.com>
2020-10-27 21:37:26 -0300

    refactor feature engineer

---
+++ scripts/feature_engineer.R
@@ -0,0 +1,36 @@
+#!/usr/bin/env Rscript
+
+# borro todo
+rm( list=ls() )
+
+require(data.table)
+require(rutiles)
+
+args = commandArgs(trailingOnly=TRUE)
+
+# PARA DEBUG
+# args = c('201908', '201909', '201911', '2', '~/Documentos/maestria-dm/dm-eyf/datasets/paquete_premium_201906_202001.txt.gz', '~/Documentos/maestria-dm/dm-eyf/kaggle/ranger_basico.csv')
+
+if (  length(args) != 6) {
+  stop("Tienen que ser 4 parametros:
+  1: mes desde: 202001, 201911, ...
+  2: ventana historico: 0, 1, 2, 3 ... (0 = sin historico)
+  3: path dataset entrada: '~/paquete_premium_201906_202001.txt.gz'
+  4: path de salida: '~/feature_eng_paquete_premium_201906_202001.txt.gz'", call.=FALSE)
+}
+
+foto_mes_desde = as.integer(args[1])
+ventana_historico = as.integer(args[2])
+dataset_path = args[3]
+path_salida = args[4]
+
+dataset = levantar_clientes(path = dataset_path)
+
+dataset = rutiles::feature_eng(dataset, historico_desde = foto_mes_desde, ventana_historico = ventana_historico)
+
+if (path_salida != '-'){
+  fwrite(dataset, file = path_salida, sep = '\t')
+}
+
+rm(list=ls())
+gc()
