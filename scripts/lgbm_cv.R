71085a0592119d34b98d33f7f37ad5a966143d9f
manu <m.iglesias91@gmail.com>
2020-11-20 21:56:55 -0300

    agregado prob2clase

--- scripts/lgbm_cv.R
+++ scripts/lgbm_cv.R
@@ -6,25 +6,27 @@ rm( list=ls() )
 args = commandArgs(trailingOnly=TRUE)
 
 # PARA DEBUG
-args = c('201909', '201911', '202001', '50', '0.001', '100', '0.01', '0.01', '0.01', '0.01', '1', '-', '50', '~/Documentos/maestria-dm/dm-eyf/datasets/paquete_premium_201906_202001.txt.gz', '~/Documentos/maestria-dm/dm-eyf/kaggle/lgbm_basico.csv')
+args = c('201909', '201911', '201905-202003-201805', '202001', '50', '0.001', '100', '0.01', '0.01', '0.01', '0.01', '1', '-', '50', '~/Documentos/maestria-dm/dm-eyf/datasets/paquete_premium_201906_202001.txt.gz', '~/Documentos/maestria-dm/dm-eyf/kaggle/lgbm_basico.csv')
 
-if (  length(args) != 15) {
-  stop("Tienen que ser 15 parametros:
+if (  length(args) != 16) {
+  stop("Tienen que ser 16 parametros:
   1: mes entrenamiento 'desde'
   2: mes entrenamiento 'hasta'
-  3: mes de evaluacion: 202001, 201911, ...
-  4: numero de iteraciones: 100, 20, ...
-  5: learning rate: 0.01, 0.001, 0.0005, ...
-  6: hojas: 50, 100, ...
-  7: feature extraction: 0.01, 0.001, 0.0005, ...
-  8: main gain to split: 0.01, 0.001, 0.0005, ...
-  9: lambda1: 0.01, 0.001, 0.0005, ...
-  10: lambda2: 0.01, 0.001, 0.0005, ...
-  11: log: -1, 0, 1, 2, ...
-  12: path features importantes: '~/features_procesadas.txt'
-  13: top features importantes a usar: 10, 20, 100, ...
-  14: path dataset entrada: '~/paquete_premium_201906_202001.txt.gz'
-  15: path de salida: '~/lgbm.csv'", call.=FALSE)
+  3: meses de validacion: 201905-202003-201805-...
+  4: mes de evaluacion: 202001, 201911, ...
+  5: numero de iteraciones: 100, 20, ...
+  6: learning rate: 0.01, 0.001, 0.0005, ...
+  7: hojas: 50, 100, ...
+  8: feature extraction: 0.01, 0.001, 0.0005, ...
+  9: main gain to split: 0.01, 0.001, 0.0005, ...
+  10: lambda1: 0.01, 0.001, 0.0005, ...
+  11: lambda2: 0.01, 0.001, 0.0005, ...
+  12: log: -1, 0, 1, 2, ...
+  13: path features importantes: '~/features_procesadas.txt'
+  14: top features importantes a usar: 10, 20, 100, ...
+  15: top features importantes a usar: 10, 20, 100, ...
+  16: path dataset entrada: '~/paquete_premium_201906_202001.txt.gz'
+  17: path de salida: '~/lgbm.csv'", call.=FALSE)
 }
 
 require(data.table)
@@ -34,19 +36,21 @@ require(rsample)
 
 foto_mes_desde = as.integer(args[1])
 foto_mes_hasta = as.integer(args[2])
-foto_mes_evaluacion = as.integer(args[3])
-iteraciones = as.integer(args[4])
-learning_rate = as.double(args[5])
-hojas = as.integer(args[6])
-feature_extraction = as.double(args[7])
-min_gain_to_split = as.double(args[8])
-lambda1 = as.double(args[9])
-lambda2 = as.double(args[10])
-log = as.integer(args[11])
-path_features = args[12]
-top_features = as.integer(args[13])
-dataset_path = args[14]
-path_salida = args[15]
+foto_meses_validacion = as.integer(strsplit(args[3], '-')[[1]])
+foto_mes_evaluacion = as.integer(args[4])
+iteraciones = as.integer(args[5])
+learning_rate = as.double(args[6])
+hojas = as.integer(args[7])
+feature_extraction = as.double(args[8])
+min_gain_to_split = as.double(args[9])
+lambda1 = as.double(args[10])
+lambda2 = as.double(args[11])
+log = as.integer(args[12])
+path_features = args[13]
+top_features = as.integer(args[14])
+undersampling = as.numeric(args[15])
+dataset_path = args[16]
+path_salida = args[17]
 
 dataset = levantar_clientes(path = dataset_path)
 
---
+++ scripts/prob2clase.R
@@ -0,0 +1,26 @@
+#!/usr/bin/env Rscript
+
+# borro todo
+rm( list=ls() )
+
+args = commandArgs(trailingOnly=TRUE)
+
+# PARA DEBUG
+# args = c('0.025', '~/repos/dmeyf/path.probs')
+
+if (length(args) != 2) {
+  stop("Tienen que ser 18 parametros:
+  1: prob de corte: 0.025, 0.05, 0.2, ...
+  2: path probabilidades", call.=FALSE)
+}
+
+library(data.table)
+library(rutiles)
+
+path_prob = args[1]
+prob_de_corte = as.numeric(args[2])
+path_salida = paste0(path_prob,'.', args[2], '.csv')
+
+probs = fread(path_prob, sep = ',')
+
+rutiles::kaggle_csv(clientes = probs[, numero_de_cliente], estimulos = as.integer(probs[,prob] > prob_de_corte), path = path_salida)
