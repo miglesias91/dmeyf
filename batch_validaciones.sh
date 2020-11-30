f4491b058e7d2e14dc2930b09e45b352292f867b
manu <m.iglesias91@gmail.com>
2020-11-30 11:07:51 -0300

    batch validaciones

--- batch_validaciones.sh
+++ batch_validaciones.sh
@@ -1,7 +1,13 @@
 !#/bin/bash
 
-Rscript --vanilla ~/repos/dmeyf/scripts/lgbm_vali.R 201707 202003 201805-201905-202003-201707-201911-201802 447 min_data_in_leaf=1000-feature_fraction=0.25-learning_rate=0.02 0.2_0.2_1 2 ~/repos/dmeyf/features-importantes-lgbm/features_standars.txt 140 0.05 ~/buckets/b1/datasetsOri/paquete_premium_final.txt.gz ~/buckets/b1/work/validaciones/lgbm.txt &&
+Rscript --vanilla ~/repos/dmeyf/scripts/lgbm_vali.R 201801 202003 201805-201905-202003-201707-201911-201802 447 min_data_in_leaf=1000-feature_fraction=0.25-learning_rate=0.02 0.2_0.2_1 2 ~/repos/dmeyf/features-importantes-lgbm/minmax_hist12.txt 400 0.05 ~/buckets/b1/dataset/minmax_hist12.txt.gz ~/buckets/b1/work/validaciones/lgbm.txt &&
 
-Rscript --vanilla ~/repos/dmeyf/scripts/lgbm_vali.R 201707 202003 201805-201905-202003-201707-201911-201802 447 min_data_in_leaf=1000-feature_fraction=0.25-learning_rate=0.02 0.2_0.2_1 2 ~/repos/dmeyf/features-importantes-lgbm/features_minmax_historicos6meses_paquete_final.txt 400 0.05 ~/buckets/b1/datasets/MinMaxTarjetas_Historico6MesesDesde202005_paquete_final.txt.gz ~/buckets/b1/work/validaciones/lgbm.txt &&
+Rscript --vanilla ~/repos/dmeyf/scripts/lgbm_vali.R 201801 202003 201805-201905-202003-201707-201911-201802 447 min_data_in_leaf=1000-feature_fraction=0.25-learning_rate=0.02 0.2_0.2_1 2 ~/repos/dmeyf/features-importantes-lgbm/minmax_hist12_lags_deltas.txt 400 0.05 ~/buckets/b1/dataset/minmax_hist12_lags_deltas.txt.gz ~/buckets/b1/work/validaciones/lgbm.txt &&
 
-Rscript --vanilla ~/repos/dmeyf/scripts/lgbm_vali.R 201707 202003 201805-201905-202003-201707-201911-201802 447 min_data_in_leaf=1000-feature_fraction=0.25-learning_rate=0.02 0.2_0.2_1 2 ~/repos/dmeyf/features-importantes-lgbm/minmax_lagdeltas_hist6meses.txt 400 0.05 ~/buckets/b1/datasets/minmax_lags_deltas_hist6meses_final.txt.gz ~/buckets/b1/work/validaciones/lgbm.txt
+Rscript --vanilla ~/repos/dmeyf/scripts/lgbm_vali.R 201710 202003 201805-201905-202003-201707-201911-201802 447 min_data_in_leaf=1000-feature_fraction=0.25-learning_rate=0.02 0.2_0.2_1 2 ~/repos/dmeyf/features-importantes-lgbm/minmax_hist9.txt 400 0.05 ~/buckets/b1/dataset/minmax_hist9.txt.gz ~/buckets/b1/work/validaciones/lgbm.txt &&
+
+Rscript --vanilla ~/repos/dmeyf/scripts/lgbm_vali.R 201710 202003 201805-201905-202003-201707-201911-201802 447 min_data_in_leaf=1000-feature_fraction=0.25-learning_rate=0.02 0.2_0.2_1 2 ~/repos/dmeyf/features-importantes-lgbm/minmax_hist9_lags_deltas.txt 400 0.05 ~/buckets/b1/dataset/minmax_hist9_lags_deltas.txt.gz ~/buckets/b1/work/validaciones/lgbm.txt &&
+
+Rscript --vanilla ~/repos/dmeyf/scripts/lgbm_vali.R 201704 202003 201805-201905-202003-201707-201911-201802 447 min_data_in_leaf=1000-feature_fraction=0.25-learning_rate=0.02 0.2_0.2_1 2 ~/repos/dmeyf/features-importantes-lgbm/minmax_hist3.txt 400 0.05 ~/buckets/b1/dataset/minmax_hist3.txt.gz ~/buckets/b1/work/validaciones/lgbm.txt &&
+
+Rscript --vanilla ~/repos/dmeyf/scripts/lgbm_vali.R 201704 202003 201805-201905-202003-201707-201911-201802 447 min_data_in_leaf=1000-feature_fraction=0.25-learning_rate=0.02 0.2_0.2_1 2 ~/repos/dmeyf/features-importantes-lgbm/minmax_hist3_lags_deltas.txt 400 0.05 ~/buckets/b1/dataset/minmax_hist3_lags_deltas.txt.gz ~/buckets/b1/work/validaciones/lgbm.txt
